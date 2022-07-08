34b14ff253f40f931519e153882df2fea0a70290
Michael Tong <michaelyuyangtong@gmail.com>
2022-07-08 19:44:47 +0800

    Added top navbar

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,14 @@
+import Head from "next/head";
+import Navbar from "../components/Navbar";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Head>
+        <title>FocalTimer</title>
+      </Head>
+      <Navbar />
+      <div className="mx-auto mt-24 w-full max-w-7xl"></div>
+    </>
+  );
 }
